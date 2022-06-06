de1548204d8e4452366d6f8841bd00f6308e39e7
Maddy <madisonschaaf48@gmail.com>
2022-06-06 12:42:48 -0600

    Fixed linked projects

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -13,49 +13,49 @@ function Portfolio() {
         <div class="projects">
 
             <div class="single-project">
-               <h4> <a href="https://code-prep-project.herokuapp.com/">
-                    <img class= "w-60 h-50" src={codePrep} target="_blank" alt="Website example of Code Prep."></img>
+               <h4> <a href="https://code-prep-project.herokuapp.com/" target="_blank">
+                    <img class= "w-60 h-50"  src={codePrep}  alt="Website example of Code Prep."></img>
                 Code Prep</a>
                 </h4>
                 <p>Node.js, Sequelize, Handlebars</p>
             </div>
 
             <div class="single-project">
-               <h4> <a href="https://noahslusher.github.io/NP-trip-planner/">
-                    <img class= "w-60 h-40" src={parkAdvisor} target="_blank" alt="Website example of Park Advisor."></img>
+               <h4> <a href="https://noahslusher.github.io/NP-trip-planner/"  target="_blank">
+                    <img class= "w-60 h-40" src={parkAdvisor} alt="Website example of Park Advisor."></img>
                 Park Advisor</a>
                 </h4>
                 <p>HTML, Javascript, CSS, Tailwind</p>
             </div>
 
             <div class="single-project">
-               <h4> <a href="https://note-taker-ms.herokuapp.com/">
-                    <img class= "w-60 h-40" src={noteTaker} target="_blank" alt="Website example of Note Taker."></img>
+               <h4> <a href="https://note-taker-ms.herokuapp.com/"  target="_blank" >
+                    <img class= "w-60 h-40" src={noteTaker} alt="Website example of Note Taker."></img>
                 Note Taker</a>
                 </h4>
                 <p>Node.js, Javascript, CSS</p>
             </div>
 
             <div class="single-project">
-               <h4> <a href="https://mschaaf17.github.io/Weather-Dashboard/">
-                    <img class= "w-60 h-40" src={weather} target="_blank" alt="Website example of weather dashboard."></img>
-                Note Taker</a>
+               <h4> <a href="https://mschaaf17.github.io/Weather-Dashboard/"  target="_blank" >
+                    <img class= "w-60 h-40" src={weather} alt="Website example of weather dashboard."></img>
+                Weather Dashboard</a>
                 </h4>
                 <p>HTML, Javascript, CSS</p>
             </div>
 
             <div class="single-project">
-               <h4> <a href="https://mschaaf17.github.io/JavaScript-Quiz/">
-                    <img class= "w-60 h-40" src={quiz}  target="_blank" alt="Website example of javascript quiz."></img>
-                Note Taker</a>
+               <h4> <a href="https://mschaaf17.github.io/JavaScript-Quiz/"  target="_blank" >
+                    <img class= "w-60 h-40" src={quiz} alt="Website example of javascript quiz."></img>
+                Javascript Quiz</a>
                 </h4>
                 <p>HTML, Javascript, CSS</p>
             </div>
 
             <div class="single-project">
-               <h4> <a href="https://mschaaf17.github.io/Password-generator/">
-                    <img class= "w-60 h-40" src={passwordGenerator} target="_blank" alt="Website example of password generator."></img>
-                Note Taker</a>
+               <h4> <a href="https://mschaaf17.github.io/Password-generator/"  target="_blank" >
+                    <img class= "w-60 h-40" src={passwordGenerator} alt="Website example of password generator."></img>
+                Password Generator</a>
                 </h4>
                 <p>HTML, Javascript, CSS</p>
             </div>
